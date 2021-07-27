9710562d4c0719cca25531db15add4905fda82c0
lichellegaerlan <lichellemaegaerlan@gmail.com>
2021-07-27 10:22:26 -0700

    testing

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -1,12 +1,23 @@
 import React from 'react';
 
+import './Landing.css';
+
 import { Container, Row, Col } from 'react-bootstrap';
 
 export default props => {
     return(
         <div>
-            <b>hello</b> <br/>
-            <i>testing</i>
+            <Container>
+                <Row>
+                    What would you like to do?
+                </Row>
+
+                <Row>
+                    <Col>
+                        <button className="pls">Pick Date</button>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
